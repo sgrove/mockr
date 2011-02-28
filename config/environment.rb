b93c46489671cd2ed5149232d77a96d62f06f1c8
Sean Grove <sean@fakecoolguys.com>
2011-02-27 16:31:36 -0800

    Pick up the Bushido domain if possible

--- config/environment.rb
+++ config/environment.rb
@@ -58,5 +58,5 @@ Rails::Initializer.run do |config|
   # Make Active Record use UTC-base instead of local time
   # config.active_record.default_timezone = :utc
 
-  config.action_mailer.default_url_options = {:host => (ENV['APP_HOST'] || "mockr")}
+  config.action_mailer.default_url_options = {:host => (ENV['BUSHIDO_DOMAIN'] || "mockr")}
 end
