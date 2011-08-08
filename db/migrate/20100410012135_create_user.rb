40a0c7047ac0542ef3bf02e0429990f0aaac5317
Akash Manohar J <akash@akash.im>
2011-08-08 23:12:23 +0530

    added note in migration; fixed load paths in script/*
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- db/migrate/20100410012135_create_user.rb
+++ db/migrate/20100410012135_create_user.rb
@@ -20,17 +20,22 @@ class CreateUser < ActiveRecord::Migration
       t.timestamps
     end
 
+
     case ActiveRecord::Base.connection.adapter_name
       when "MySQL"
         execute("ALTER TABLE users MODIFY COLUMN facebook_uid bigint NOT NULL");
       when "PostgreSQL"
         execute("ALTER TABLE users ALTER COLUMN facebook_uid TYPE bigint")
       else
-        raise "Don't know how to change column to bigint"
+        # raise "Don't know how to change column to bigint"
+        puts "*#"*30
+        puts "CAUTION: USING SQLITE. NOT CHANGING FACEBOOK_UID TO BIGINT"
+        puts "*#"*30
     end
 
     add_index "users", "facebook_uid", :unique => true
-  end
+
+end
 
   def self.down
     drop_table :users
