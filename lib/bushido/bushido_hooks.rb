4abd3bd3e8117e80f3bcef1ee701767973190b6c
Sean Grove <s@bushi.do>
2011-11-25 18:15:30 -0800

    updating bushido hooks

--- lib/bushido/bushido_hooks.rb
+++ lib/bushido/bushido_hooks.rb
@@ -30,29 +30,44 @@ module MockrBushido
 
 
       ::Bushido::Data.listen('mail.received') do |payload, event|
+        puts "Got some mail!"
         mail = payload["mail"]
-        attachments = []
-        mail["attachment_count"].to_i.times do |counter|
-          attachments << mail["attachment_#{counter}"]
-        end
-        attachmets.flatten!
+        puts "Here's the mail: #{mail.inspect}"
+        # attachments = []
+        # puts "collecting attachments!"
+        # mail["attachment_count"].to_i.times do |counter|
+        #   attachments << mail["attachment_#{counter}"]
+        # end
+        # attachmets.flatten!
 
+        puts "Getting the subject:"
         subject = mail["subject"]
+        puts "\tsubject: #{subject}"
+        puts "Getting the project_title:"
         project_title = subject.split(":").first
+        puts "\tproject_title: #{project_title}"
+        puts "Getting the mock_list_title"
         mock_list_title = subject.split(":").last.split("#").first
+        puts "\tmock_list_title: #{mock_list_title}"
+        puts "Getting the mock_version:"
         mock_version = subject.split(":").last.split("#").last.split("comment").first.to_i
+        puts "\tmock_version: #{mock_version}"
+        puts "Getting the comment_id:"
         comment_id =  subject.split(":").last.split("#").last.split("comment").last.to_i
+        puts "\tcomment_id: #{comment_id}"
 
         project = Project.find_or_create_by_title project_title
+        puts "project: #{project.inspect}"
         mock_list = Project.mock_lists.find_or_create_by_title mock_list_title
+        puts "mock_list: #{mock_list.inspect}"
 
         mock = mock_list.mocks.find_by_version mock_version if mock_version != 0
         comment = mock.comments.find(comment_id) if comment_id != 0
 
         puts "Project:  #{project.inspect}"
         puts "MockList: #{mock_list.inspect}"
-        puts "Mock:     #{mock.inspect}"
-        puts "Comment   #{comment.inspect}"
+        puts "Mock:     #{mock.inspect}" if mock
+        puts "Comment   #{comment.inspect}" if comment
       end
     end
   end
