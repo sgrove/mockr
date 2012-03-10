5bd75a9b7ebf58b28b95b22c0389135ab4dafdbf
Sean Grove <s@bushi.do>
2012-03-09 16:59:28 -0800

    Fixes the User hooks

--- lib/bushido/hooks/user_hooks.rb
+++ lib/bushido/hooks/user_hooks.rb
@@ -1,7 +1,7 @@
 class BushidoUserHooks < Bushido::EventObserver
   def user_added
-    user.create(:email  => params['data']['email'],
-                :ido_id => params['data']['ido_id'])
+    User.create!(:email  => params['data']['email'],
+                 :ido_id => params['data']['ido_id'])
   end
 
   def user_removed
